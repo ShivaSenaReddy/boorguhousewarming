2d76adbb07a313f22c08c238130c8a2f46b4192d
JAGAPATHI Shiva Sena Reddy <shiva.sena.reddy.jagapathi@hexagon.com>
2025-11-10 14:47:05 +0530

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("rsvpBtn").addEventListener("click", () => {
+  alert("Thank you for confirming! We’re excited to have you 🎉");
+});
+
+document.getElementById("locationBtn").addEventListener("click", () => {
+  window.open("https://maps.app.goo.gl/EGvwLoRhbPBnj5Lv8?g_st=iw", "_blank");
+});
